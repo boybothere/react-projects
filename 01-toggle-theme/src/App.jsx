a00e44574733bc5a92c012c8366c05a68365830f
boybothere <boybothere@gmail.com>
2025-07-05 20:57:44 +0530

    feat: finish toggle theme project

--- 01-toggle-theme/src/App.jsx
+++ 01-toggle-theme/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState} from 'react'
 import './App.css'
+import { Toggle } from './components/Toggle.jsx'
+import useLocalStorage from "use-local-storage"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("IsDark", preference);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" data-theme = {isDark ? "dark" : "light"}>
+      <Toggle 
+        handleChange={()=>setIsDark(!isDark)}
+        isChecked={isDark}
+      />
+      <h1 className="title">Hello World!</h1>
+      <div className="box">
+        <h2>This is a box</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
